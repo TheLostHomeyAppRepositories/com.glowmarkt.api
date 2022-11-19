47b5d343088d87d3cc86af535f5f8a3a6ae1d51f
jmdraper <github@12mail.me>
2022-11-19 11:40:50 +0000

    Added smart meter driver and pairing

--- drivers/smart-meter/driver.js
+++ drivers/smart-meter/driver.js
@@ -18,16 +18,15 @@ class MyDriver extends Driver {
    */
   async onPairListDevices() {
     return [
-      // Example device data, note that `store` is optional
-      // {
-      //   name: 'My Device',
-      //   data: {
-      //     id: 'my-device',
-      //   },
-      //   store: {
-      //     address: '127.0.0.1',
-      //   },
-      // },
+      {
+        name: 'Smart Meter',
+        data: {
+          id: 'my-smart-meter',
+        },
+        store: {
+          token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbkhhc2giOiJiZmVjMzc3ZTVlYzMwYmNmMmJjNGMzZGMyOGU3ZjIyZmUzNzdiNTIxOWMzMzc4YWY4YmQ5MzFhMmQ2OWE3ODdlODQyNmQ4MTFjYjc1NzcyZGQ5OGM0ZTU4ZDgzODNiODkiLCJ2ZXJzaW9uIjoiMS4xIiwiaWF0IjoxNjY3NTIwMDAwLCJleHAiOjE3MTUwNDAwMDB9.KjcQdzyGRdVhJzpWMGKVNHeBhhFwoYTh75zzZ75oarU',
+        },
+      },
     ];
   }
 
